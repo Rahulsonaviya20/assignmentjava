c2a49412a911a3caad46e23d05e3e85071ef575a
Rahulsonaviya20 <rahulsonaviya9429@gmail.com>
2025-02-05 09:26:23 +0530

    Add files via upload

---
+++ abstraction.java
@@ -0,0 +1,25 @@
+public class Abstractions {
+    public static void main(String[] args) {
+        Horse h = new Horse();
+        h.eat();
+        h.walk();
+
+        Lion l = new Lion1(); // Fixed class name capitalization
+        l.eat();
+        l.walk();
+    }
+}
+
+abstract class Animal1 {
+    void eat() {
+        System.out.println("animal eats");
+    }
+
+    abstract void walk();
+}
+
+class Horse extends Animal1 {
+    void walk() {
+        System.out.println("walks on 4 legs");
+    }
+}
